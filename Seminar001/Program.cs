27b4aff90f3d89a86401cb1646b76bd530fb43cc
Aleksey <alekseymamaev99@mail.ru>
2022-09-27 18:18:05 +0300

    Выполнил задания по 1 семинару

--- Seminar001/Program.cs
+++ Seminar001/Program.cs
@@ -56,10 +56,11 @@
 
 //                                                                       Задача 8
 
-Console.WriteLine("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 0; i <= N; i = i+2)
-{
-    Console.WriteLine(i);
-}
+// for (int i = 0; i <= N; i = i+2)
+// {
+//     Console.WriteLine(i);
+// }
+
