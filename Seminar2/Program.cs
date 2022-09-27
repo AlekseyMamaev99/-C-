6300976fa5834ff168f0947efb4be89242f13d24
Aleksey <alekseymamaev99@mail.ru>
2022-09-28 00:05:55 +0300

    Решил задачи13 и 15

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -3,8 +3,45 @@
 //                                                                      Задача 10
 
 
-int n = new Random().Next(100,1000);
-Console.WriteLine(n);
+// int n = new Random().Next(100,1000);
+// Console.WriteLine(n);
 
-int second = (n % 100) / 10;
-Console.WriteLine(second);
+// int second = (n % 100) / 10;
+// Console.WriteLine(second);
+
+//                                                                      Задача 13
+
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n >= 100)
+// {
+//     string x = Convert.ToString(n);
+    
+//     Console.WriteLine(x[2]);
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет!");
+// }
+
+
+
+//                                                                      Задача 15 
+
+
+// Console.WriteLine("Введите цифру дня недели: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (1 <= n && n <= 5)
+// {
+//     Console.WriteLine("NO!");
+// }
+// else if (n == 6 || n == 7)
+// {
+//     Console.WriteLine("YES!!!");
+// }
+// else
+// {
+//     Console.WriteLine("Нет такого дня недели((( Попробуйте снова!");
+// }
