d10c59ad52a5352b3644d871fd188ae724c08948
Aleksey <alekseymamaev99@mail.ru>
2022-09-27 23:39:19 +0300

    Решил задачу 10 из семинара 2

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//                                                             Домашняя работа по семинару №2
+
+//                                                                      Задача 10
+
+
+int n = new Random().Next(100,1000);
+Console.WriteLine(n);
+
+int second = (n % 100) / 10;
+Console.WriteLine(second);
