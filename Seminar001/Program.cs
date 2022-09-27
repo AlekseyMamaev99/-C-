caa82fc7ba457d153e0e39e5365716fcab1945ff
Aleksey <alekseymamaev99@mail.ru>
2022-09-27 17:07:57 +0300

    Решил задачу 2

---
+++ Seminar001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Домашняя работа по семинару №1.
+
+// Задача 2
+
+int a = 5;
+int b = 7;
+
+if (a > b )
+{
+    Console.Write("max = ");
+    Console.Write(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.Write(b);
+}
