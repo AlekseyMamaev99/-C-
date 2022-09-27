7b77e49ffcff6234014649100921429258925b6e
Aleksey <alekseymamaev99@mail.ru>
2022-09-27 17:29:35 +0300

    Решил задачу 4

--- Seminar001/Program.cs
+++ Seminar001/Program.cs
@@ -2,16 +2,40 @@
 
 // Задача 2
 
-int a = 5;
-int b = 7;
+// int a = 5;
+// int b = 7;
 
-if (a > b )
+// if (a > b )
+// {
+//     Console.Write("max = ");
+//     Console.Write(a);
+// }
+// else
+// {
+//     Console.Write("max = ");
+//     Console.Write(b);
+// }
+
+
+// Задача 4
+
+int a = 44;
+int b = 5;
+int c = 78;
+
+int max = a;
+
+if (b > a && b > c)
 {
-    Console.Write("max = ");
-    Console.Write(a);
+     max = b;
+}
+else if (c > a && c > b)
+{
+     max = c;
 }
 else
 {
-    Console.Write("max = ");
-    Console.Write(b);
+     max = a;
 }
+
+Console.WriteLine(max);
