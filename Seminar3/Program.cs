c3f9750952a96ff41d47b52dcca61bc9d463def5
Aleksey <alekseymamaev99@mail.ru>
2022-10-01 18:49:20 +0300

    Решил задачу 19

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//                                                          Практическое задание по семинару 3
+//                                                                      Задача 19
+
+Console.WriteLine("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string txt = Convert.ToString(n);
+
+if (n < 10000 || n >= 100000 )
+    Console.WriteLine("Это не пятизначное число! Попробуйте снова!");
+
+else if (txt [0] == txt [4] && txt [1] == txt [3])
+    Console.WriteLine("Это число полиндром!");
+        
+else 
+    Console.WriteLine("НЕ полиндром");
+        
+
