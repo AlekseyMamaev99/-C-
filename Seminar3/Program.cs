085a0e4a324b4e504fc5bc0cb56bd518f6c10839
Aleksey <alekseymamaev99@mail.ru>
2022-10-01 19:47:15 +0300

    Решил задачу 23

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -18,20 +18,32 @@
 
 //                                                                      Задача 21
 
-Console.WriteLine("Введите координату x1:");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату y1:");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату z1:");
-int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату x1:");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату y1:");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату z1:");
+// int z1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату x2:");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату y2:");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координату z2:");
-int z2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату x2:");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату y2:");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату z2:");
+// int z2 = Convert.ToInt32(Console.ReadLine());
 
-double s = Math.Sqrt((x2 - x1) * (x2 -x1) + (y1 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+// double s = Math.Sqrt((x2 - x1) * (x2 -x1) + (y1 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
 
-Console.WriteLine("Результат: " + s);
+// Console.WriteLine("Результат: " + s);
+
+//                                                                      Задача 23
+
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int kub = 0;
+
+// for (int i = 1; i <= n; i++)
+// {
+//     kub = i * i * i;
+//     Console.WriteLine(kub + ";");
+// }
