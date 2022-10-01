61d31cae6ede54a0216e1932a4dd3595b4fa648b
Aleksey <alekseymamaev99@mail.ru>
2022-10-02 00:00:35 +0300

    Выполнил задания по семинару 4

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -16,3 +16,45 @@
 
 //                                                                      Задача 27
 
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+string m = Convert.ToString(n);
+
+for (int i = 0; i < m.Length; i++)
+{
+    int x = Convert.ToInt32(Convert.ToString(m[i]));
+    sum =sum + x;
+}
+Console.WriteLine(sum);
+
+
+//                                                                      Задача 29
+
+// int[] array = new int[8];
+// Console.WriteLine("Введите первое число: ");
+// array[0] = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число: ");
+// array[1] = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите третие число: ");
+// array[2] = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите четвертое число: ");
+// array[3] = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите пятое число: ");
+// array[4] = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите шестое число: ");
+// array[5] = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите седьмое число: ");
+// array[6] = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите восьмое число: ");
+// array[7] = Convert.ToInt32(Console.ReadLine());
+
+
+// Console.Write("[" + string.Join(",", array) + "]");
