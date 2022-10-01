6ba37a9a046a678361fbe2a52d07be45d64bedf1
Aleksey <alekseymamaev99@mail.ru>
2022-10-01 22:17:13 +0300

    Выполнил задание 25

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//                                                        Практическая работа по семинару 4
+
+//                                                                      Задача 25
+
+
+// Console.WriteLine("Введите чило: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите степень чила: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int result = 1;
+// for (int i = 1; i <= b; i++)
+// {
+//     result = result * a;
+// }
+// Console.WriteLine(result);
+
+//                                                                      Задача 27
+
