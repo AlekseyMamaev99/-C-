d1ebb7ef580c932e4bb9577cddb2ce3f8268e462
Aleksey <alekseymamaev99@mail.ru>
2022-10-01 19:01:42 +0300

    Решил задачу 21

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,18 +1,37 @@
 ﻿//                                                          Практическое задание по семинару 3
 //                                                                      Задача 19
 
-Console.WriteLine("Введите пятизначное число: ");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите пятизначное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-string txt = Convert.ToString(n);
+// string txt = Convert.ToString(n);
 
-if (n < 10000 || n >= 100000 )
-    Console.WriteLine("Это не пятизначное число! Попробуйте снова!");
+// if (n < 10000 || n >= 100000 )
+//     Console.WriteLine("Это не пятизначное число! Попробуйте снова!");
 
-else if (txt [0] == txt [4] && txt [1] == txt [3])
-    Console.WriteLine("Это число полиндром!");
+// else if (txt [0] == txt [4] && txt [1] == txt [3])
+//     Console.WriteLine("Это число полиндром!");
         
-else 
-    Console.WriteLine("НЕ полиндром");
-        
+// else 
+//     Console.WriteLine("НЕ полиндром");
 
+
+//                                                                      Задача 21
+
+Console.WriteLine("Введите координату x1:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y1:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z1:");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату x2:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y2:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z2:");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double s = Math.Sqrt((x2 - x1) * (x2 -x1) + (y1 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+
+Console.WriteLine("Результат: " + s);
