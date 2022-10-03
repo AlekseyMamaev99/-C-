1f29a2b0c11e7ddaf2cce1268ebc5a0f6077ce6b
Aleksey <alekseymamaev99@mail.ru>
2022-10-03 18:00:07 +0300

    Выполнил практическое задание по 6 семинару

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -2,34 +2,69 @@
 
 //                                                                      Задача 41
 
+// Console.Clear();
+
+// Console.WriteLine("Введите нужное количество элементов массива :");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[m];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.WriteLine($"Введите {i + 1} элемент массива: ");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+
+// int CountBolsheZero(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             count = count + 1;
+//         }
+//     }
+//     return count;
+// }
+
+// Console.WriteLine($"[" + String.Join("; ", array) + "]");
+// Console.WriteLine();
+// CountBolsheZero(array);
+// Console.WriteLine($"Элементов > 0 = [{CountBolsheZero(array)}]");
+
+
+
+//                                                                      Задача 43
+
+
+
 Console.Clear();
 
-Console.WriteLine("Введите нужное количество элементов массива :");
-int m = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[m];
+Console.WriteLine("Введите переменную b1 :");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите переменную k1 :");
+double k1 = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 0; i < array.Length; i++)
+Console.WriteLine("Введите переменную b2 :");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите переменную k2 :");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (k1 != k2)
+{   
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Координаты точки пересечения: ({x};{y})");
+}
+else if (k1 == k2 && b1 == b2)
 {
-    Console.WriteLine($"Введите {i + 1} элемент массива: ");
-    array[i] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Это одна и та же прямая!");
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Эти прямые паралелльны!");
 }
 
-
-int CountBolsheZero(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] > 0)
-        {
-            count = count + 1;
-        }
-    }
-    return count;
-}
-
-Console.WriteLine($"[" + String.Join("; ", array) + "]");
-Console.WriteLine();
-CountBolsheZero(array);
-Console.WriteLine($"Элементов > 0 = [{CountBolsheZero(array)}]");
 
