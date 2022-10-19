7e478678c1b66912bb867be9f7465b2d624a0828
Aleksey <alekseymamaev99@mail.ru>
2022-10-19 17:36:19 +0300

    Выполнил задание 66 из семинара 9

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//                                                          Практическое задание по семинару 9
+
+//                                                                      Задача 66
+
+Console.Clear();
+
+int SumFromMToN(int m, int n)
+{
+    if (n == m)
+        return n;
+    return SumFromMToN(m,n - 1) + n;
+}
+
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumFromMToN(m,n));
