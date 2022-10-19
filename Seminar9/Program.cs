1e7c61fd294e49e5b53493bf32f0068e62f8051d
Aleksey <alekseymamaev99@mail.ru>
2022-10-19 17:57:59 +0300

    Выполнил практическое задание по семинару 9

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -2,18 +2,42 @@
 
 //                                                                      Задача 66
 
-Console.Clear();
+// Console.Clear();
 
-int SumFromMToN(int m, int n)
-{
-    if (n == m)
-        return n;
-    return SumFromMToN(m,n - 1) + n;
-}
+// int SumFromMToN(int m, int n)
+// {
+//     if (n == m)
+//         return n;
+//     return SumFromMToN(m,n - 1) + n;
+// }
 
-Console.WriteLine("Введите число M");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число N");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число M");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число N");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(SumFromMToN(m,n));
+// Console.WriteLine(SumFromMToN(m,n));
+
+
+//                                                                      Задача 68
+
+// Console.Clear();
+
+// int AkkermanF(int m, int n)
+// {
+//     if (m == 0)
+//         return n + 1;
+//     if (n == 0)
+//         return AkkermanF(m - 1,1);
+//     return AkkermanF(m - 1, AkkermanF(m,n - 1));
+    
+// }
+// Console.WriteLine("Введите число M");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число N");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+// if (m < 0 || n < 0)
+//     Console.WriteLine("Введите неотрицательные числа!");
+// else
+//     Console.WriteLine("Вычесление по функции Аккермана = " + AkkermanF(m,n));
